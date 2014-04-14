f927f589235790c98b58db36c8aa1ddd8bc4da02
enverveli <enverveli@gmail.com>
2014-04-14 10:51:27 +0300

    + local properties, deploy batch script file, intellij project files and target folder are added to git ignored files

--- deploy.bat
+++
@@ -1,4 +0,0 @@
-rd /S /Q C:\Tomcat\apache-tomcat-6.0.37\webapps\uns
-rd /S /Q C:\Tomcat\apache-tomcat-6.0.37\work\Catalina\localhost\uns
-del /S /Q C:\Tomcat\apache-tomcat-6.0.37\conf\Catalina\localhost\uns.xml
-copy /Y .\target\uns.war C:\Tomcat\apache-tomcat-6.0.37\webapps\uns.war
