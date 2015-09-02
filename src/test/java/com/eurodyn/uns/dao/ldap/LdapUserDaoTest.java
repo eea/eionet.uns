9c1ee48d3f3b139858512e7d4518f0ba5049b4da
gsf-greece <gsf.greece@hotmail.com>
2015-09-02 12:15:14 +0300

    Added test, code refactoring. Refs #28289

---
+++ src/test/java/com/eurodyn/uns/dao/ldap/LdapUserDaoTest.java
@@ -0,0 +1,86 @@
+package com.eurodyn.uns.dao.ldap;
+
+import com.eurodyn.uns.model.User;
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.persist.LDAPDNField;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.ldap.Control;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.PagedResultsControl;
+import java.net.InetAddress;
+import java.util.Hashtable;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author George Sofianos
+ */
+
+public class LdapUserDaoTest {
+
+  private InMemoryDirectoryServer ds;
+  @LDAPDNField
+  private String baseDn = "o=Eionet, l=EUROPE";
+
+  /**
+   * Sets up in memory LDAP Server
+   * @throws Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    InMemoryListenerConfig listenerConfig = new InMemoryListenerConfig("test",InetAddress.getLocalHost() , 10389, null, null, null);
+
+    InMemoryDirectoryServerConfig config = new InMemoryDirectoryServerConfig(baseDn);
+    config.setSchema(null);
+    config.addAdditionalBindCredentials("cn=Test User", "pass");
+
+    config.setListenerConfigs(listenerConfig);
+    ds = new InMemoryDirectoryServer(config);
+    ds.importFromLDIF(false, getClass().getResource("/init.ldif").getPath());
+    ds.startListening();
+  }
+
+  /**
+   * Tests if an LDAP connection can be established and a search for users method can be completed.
+   * @throws Exception
+   */
+  @Test
+  public void testFindAllUsers() throws Exception {
+    LdapUserDao ldap = mock(LdapUserDao.class);
+
+    Hashtable env = new Hashtable();
+    env.put(LdapContext.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+    env.put(LdapContext.PROVIDER_URL, "ldap://localhost:10389");
+    env.put(LdapContext.SECURITY_AUTHENTICATION, "simple");
+    env.put(LdapContext.SECURITY_PRINCIPAL, "cn=Test User");
+    env.put(LdapContext.SECURITY_CREDENTIALS, "pass");
+    LdapContext ctx = new InitialLdapContext(env, null);
+    ctx.setRequestControls(new Control[]{
+            new PagedResultsControl(50, Control.CRITICAL)
+    });
+
+    when(ldap.getPagedLdapContext()).thenReturn(ctx);
+    when(ldap.findAllUsers()).thenCallRealMethod();
+    List<User> users = ldap.findAllUsers();
+    assertTrue("Expecting some users", users.size() > 0);
+  }
+
+  /**
+   * Shuts down in memory LDAP Server
+   * @throws Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+    ds.shutDown(true);
+  }
+}
---
+++ src/test/java/com/eurodyn/uns/service/daemons/userupdater/UserUtilTest.java
@@ -0,0 +1,45 @@
+package com.eurodyn.uns.service.daemons.userupdater;
+
+import com.eurodyn.uns.model.DeliveryAddress;
+import com.eurodyn.uns.model.DeliveryType;
+import com.eurodyn.uns.model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author George Sofianos
+ */
+public class UserUtilTest {
+
+  /**
+   * Tests if two users are different
+   * @throws Exception
+   */
+  @Test
+  public void testEqualUsers() throws Exception {
+    User user1 = new User("test1");
+    Map addrs1 = new HashMap();
+    DeliveryType mailType = new DeliveryType(DeliveryType.EMAIL);
+    DeliveryAddress addr1 = new DeliveryAddress(1, "mock@mockmail.com");
+    addr1.setDeliveryType(mailType);
+    addrs1.put(1, addr1);
+    user1.setDeliveryAddresses(addrs1);
+    user1.setDisabledFlag(false);
+    user1.setFullName("Test User1");
+    User user2 = new User("test2");
+    Map addrs2 = new HashMap();
+    addrs2.put(1, addr1);
+    user2.setDeliveryAddresses(addrs2);
+    user2.setFullName("Test User2");
+    user2.setDisabledFlag(true);
+    assertTrue("Users must not be equal", UserUtil.equalUsers(user1,user2) == false);
+  }
+
+}
