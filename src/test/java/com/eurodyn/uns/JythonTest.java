d60167ff1e99e662f3720b442c69418a8a9275f6
George Sofianos <gso@eworx.gr>
2017-09-29 12:44:24 +0300

    Reverted Jython version. Refs #86798

--- src/test/java/com/eurodyn/uns/JythonTest.java
+++ src/test/java/com/eurodyn/uns/JythonTest.java
@@ -1,21 +1,22 @@
 package com.eurodyn.uns;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import org.python.core.Py;
 import org.python.core.PyObject;
-import org.python.core.PyInteger;
-import org.python.core.PyException;
 import org.python.core.PyString;
 import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Tests for the Python templates.
  */
 public class JythonTest {
+
+    private Logger LOGGER = LoggerFactory.getLogger(JythonTest.class);
 
     /**
      * Test 2 + 2 is 4
@@ -35,6 +36,23 @@ public class JythonTest {
         PythonInterpreter interp =  new PythonInterpreter(null, new PySystemState());;
         PySystemState sys = Py.getSystemState();
         interp.exec("import re");
+    }
+
+    @Test
+    public void testUnicode() {
+        PythonInterpreter interpreter = new PythonInterpreter();
+
+        interpreter.set("mystr", new PyString("Søren Roug")); // Test with char from Latin-1
+        PyObject myStr = interpreter.get("mystr");
+        LOGGER.info(String.valueOf(myStr));
+
+        interpreter.set("mystr", new PyString("€ as Java String"));
+        myStr = interpreter.get("mystr");
+        LOGGER.info(String.valueOf(myStr));
+
+        interpreter.exec("mystr = u'€ as Python UNICODE'");
+        myStr = interpreter.get("mystr");
+        LOGGER.info(String.valueOf(myStr));
     }
 
 }
