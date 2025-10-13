dd973d848efffd98b9af77df7297c9691d98d084
js <js@eworx.gr>
2025-10-13 12:53:39 +0300

    Refs #292755 libraries upgrades

--- src/main/java/com/eurodyn/uns/util/URLReader.java
+++ src/main/java/com/eurodyn/uns/util/URLReader.java
@@ -21,118 +21,79 @@
 
 package com.eurodyn.uns.util;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.eurodyn.uns.Properties;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-
-
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.util.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Utility responsible for reading data by using HTTP protocol.
- * It wraps Apache http-client library in order to provide WDS with capability of controling
+ * It wraps Apache http-client library in order to provide WDS with the capability of controlling
  * connection and socket timeouts.
  *
  */
 public class URLReader {
     private static final Logger LOGGER = LoggerFactory.getLogger(URLReader.class);
-    private static final int DEFAULT_SOCKET_TIMEOUT = 1000*10;
-    private org.apache.commons.httpclient.HttpClient client;
+    private static final int DEFAULT_SOCKET_TIMEOUT = 10;
+    private final CloseableHttpClient client;
     private static int socketTimeout;
 
     static {
         try {
-            socketTimeout=1000*Integer.parseInt(Properties.getStringProperty("channel.socket_timeout"));
+            socketTimeout = Integer.parseInt(Properties.getStringProperty("channel.socket_timeout"));
         } catch (Exception e) {
             socketTimeout = DEFAULT_SOCKET_TIMEOUT;
             LOGGER.info("Could not load socket timeout from configuration, assuming default: " + DEFAULT_SOCKET_TIMEOUT);
         }
     }
 
-
     public URLReader() {
-        client = new org.apache.commons.httpclient.HttpClient();
-        client.getHttpConnectionManager().getParams().setConnectionTimeout(5000);
-        client.getHttpConnectionManager().getParams().setSoTimeout(socketTimeout);
+        RequestConfig config = RequestConfig.custom()
+                .setRedirectsEnabled(true)
+                .setConnectionRequestTimeout(Timeout.ofSeconds(5))
+                .setResponseTimeout(Timeout.ofSeconds(socketTimeout))
+                .build();
+
+        client = HttpClients.custom()
+                .setDefaultRequestConfig(config)
+                .build();
     }
-
-
-    public ByteArrayOutputStream getContentAsOutStream(String url) throws Exception {
-        ByteArrayOutputStream result = null;
-        HttpMethod method = getMethod(url);
-        try {
-            client.executeMethod(method);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(method.getResponseBodyAsStream());
-            result = new ByteArrayOutputStream();
-            byte[] b = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = bufferedInputStream.read(b, 0, b.length)) != -1) {
-                result.write(b, 0, bytesRead);
-            }
-            result.flush();
-            result.close();
-        } catch (HttpException he) {
-            throw new Exception("Http error connecting to '" + url + "' " + he.getMessage());
-        } catch (IOException ioe) {
-            throw ioe;
-        } finally {
-            if (method != null) {
-                method.releaseConnection();
-            }
-        }
-        return result;
-    }
-
 
     public ByteArrayInputStream getContentAsInStream(String url) throws Exception {
-        byte[] content = null;
-        if(url.startsWith("file:///")){
-            String filename = url.substring(8);
-            FileInputStream fis = new FileInputStream(new File(filename));
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            Streams.drain(fis, bytes);
-            content = bytes.toByteArray();
-        } else {
-            content = getContentAsBytes(url);
-        }
-
-        return new ByteArrayInputStream(content);
+        return new ByteArrayInputStream(getContentAsBytes(url));
     }
 
+    private byte[] getContentAsBytes(String url) throws Exception {
+        if (url.startsWith("file:///")) {
+            return readFile(url.substring(8));
+        }
 
-    public byte[] getContentAsBytes(String url) throws Exception {
-        HttpMethod method = getMethod(url);
-        byte[] responseBody;
-        try {
-            client.executeMethod(method);
-            responseBody = method.getResponseBody();
-        } catch (HttpException he) {
-            throw new Exception("Http error connecting to '" + url + "' " + he.getMessage());
-        } catch (IOException ioe) {
-            throw ioe;
-        } finally {
-            if (method != null) {
-                method.releaseConnection();
+        HttpGet get = new HttpGet(url);
+        try (CloseableHttpResponse response = client.execute(get)) {
+            if (response.getEntity() != null) {
+                return EntityUtils.toByteArray(response.getEntity());
+            } else {
+                return new byte[0];
             }
         }
-        return responseBody;
     }
 
-
-    private HttpMethod getMethod(String url) {
-        HttpMethod method = null;
-        method = new GetMethod(url);
-        method.setFollowRedirects(true);
-        return method;
+    private byte[] readFile(String path) throws IOException {
+        File file = new File(path);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            return fis.readAllBytes();
+        }
     }
 
 }
